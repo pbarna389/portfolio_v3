0e6dd76dc68dcbf1b1510a7dc201dfd9fda23c79
PB <pbarna389@gmail.com>
2025-09-02 00:56:28 +0200

    03/hero - fixed the profile's width and height

--- src/components/ProfilePic/ProfilePic.tsx
+++ src/components/ProfilePic/ProfilePic.tsx
@@ -8,7 +8,7 @@ type ProfilePicProps = {
 export const ProfilePic = ({ basisNum = 50, fullyRounded = true }: ProfilePicProps) => {
 	return (
 		<div
-			className={`flex flex-col relative basis-${basisNum} justify-center items-center before:absolute before:bottom-0 before:rounded-${fullyRounded && 'full'} before:w-[calc(100vw*0.902)] before:h-[calc(100vw*0.902)] before:content-[""] before:bg-white/4 before:-z-1 *:-z-1`}
+			className={`flex flex-col relative basis-${100 / basisNum} justify-center items-center before:absolute before:bottom-0 before:rounded-${fullyRounded && 'full'} before:w-[min(calc(100vw*0.945),750px)] before:h-[min(calc(100vw*0.945),750px)] before:content-[""] before:bg-white/4 before:-z-1 sm:before:w-[min(calc(100vw*0.55),480px)] sm:before:h-[min(calc(100vw*0.55),480px)] md:before:w-[min(calc(100vw*0.5),525px)] md:before:h-[min(calc(100vw*0.5),525px)] lg:before:w-[min(calc(100vw*0.42),525px)] lg:before:h-[min(calc(100vw*0.42),525px)] *:-z-1`}
 		>
 			<img className="grayscale-100 w-6/7" src={img} />
 		</div>
--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -2,7 +2,8 @@ import { ProfilePic } from '@components'
 
 export const Hero = () => {
 	return (
-		<div>
+		<div className="flex flex-col gap-1 sm:flex sm:flex-row sm:justify-between">
+			<div className="basis-1/2">Data</div>
 			<ProfilePic basisNum={50} />
 		</div>
 	)
