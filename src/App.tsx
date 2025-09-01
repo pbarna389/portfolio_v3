0e75d306ef12fae610ca487c35fbb0a70f6f699c
PB <pbarna389@gmail.com>
2025-09-01 15:41:31 +0200

    01/setup - added custom colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-	return <h1 className="text-3xl font-bold">Portfolio Site</h1>
+	return <h1 className="text-3xl font-bold text-highlight">Portfolio Site</h1>
 }
 
 export default App
