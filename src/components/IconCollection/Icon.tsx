1402c9d088c5cf61c07758cf59bcd61753444fb7
PB <pbarna389@gmail.com>
2025-09-02 15:58:45 +0200

    05/skills - updated selectable icons

--- src/components/IconCollection/Icon.tsx
+++ src/components/IconCollection/Icon.tsx
@@ -1,8 +1,25 @@
 import { FaLinkedinIn } from 'react-icons/fa'
+import { FaReact } from 'react-icons/fa'
+import { FaHtml5 } from 'react-icons/fa'
+import { FaCss3Alt } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa6'
 import { FaDownload } from 'react-icons/fa6'
+import { SiJavascript } from 'react-icons/si'
+import { SiTypescript } from 'react-icons/si'
+import { SiTailwindcss } from 'react-icons/si'
+import { SiStrapi } from 'react-icons/si'
 
-type IconKeys = 'github' | 'linkedin' | 'download'
+type IconKeys =
+	| 'github'
+	| 'linkedin'
+	| 'download'
+	| 'react'
+	| 'javascript'
+	| 'typescript'
+	| 'html'
+	| 'css'
+	| 'tailwind'
+	| 'strapi'
 
 type IconsType<Key extends string> = {
 	[Property in Key]: React.ComponentType<React.SVGProps<SVGSVGElement>>
@@ -11,7 +28,14 @@ type IconsType<Key extends string> = {
 const Icons: IconsType<IconKeys> = {
 	github: FaGithub,
 	linkedin: FaLinkedinIn,
-	download: FaDownload
+	download: FaDownload,
+	react: FaReact,
+	javascript: SiJavascript,
+	typescript: SiTypescript,
+	html: FaHtml5,
+	css: FaCss3Alt,
+	tailwind: SiTailwindcss,
+	strapi: SiStrapi
 }
 
 type IconProps = {
