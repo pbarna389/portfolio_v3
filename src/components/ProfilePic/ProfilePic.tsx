72a0875c0b5e08c7d9b28962f364e01de2fe3aec
PB <pbarna389@gmail.com>
2025-09-02 13:01:48 +0200

    03/hero - fixed img

--- src/components/ProfilePic/ProfilePic.tsx
+++ src/components/ProfilePic/ProfilePic.tsx
@@ -7,9 +7,9 @@ type ProfilePicProps = {
 export const ProfilePic = ({ basisNum = 50 }: ProfilePicProps) => {
 	return (
 		<div
-			className={`flex flex-col relative basis-${100 / basisNum} justify-center items-center before:absolute before:bottom-0 before:rounded-full before:w-[min(calc(100vw*0.945),750px)] before:h-[min(calc(100vw*0.945),750px)] before:content-[""] before:bg-white/4 before:-z-1 sm:before:w-[min(calc(100vw*0.42),550px)] sm:before:h-[min(calc(100vw*0.42),550px)] md:before:w-[min(calc(100vw*0.47),500px)] md:before:h-[min(calc(100vw*0.47),500px)] lg:before:w-[min(calc(100vw*0.42),525px)] lg:before:h-[min(calc(100vw*0.42),525px)] *:-z-1 border-2 border-white`}
+			className={`flex flex-col relative basis-${100 / basisNum} justify-end items-center before:absolute before:bottom-0 before:rounded-full before:w-[min(calc(100vw*0.945),750px)] before:h-[min(calc(100vw*0.945),750px)] before:content-[""] before:bg-white/4 before:-z-1 sm:before:w-[min(calc(100vw*0.42),550px)] sm:before:h-[min(calc(100vw*0.42),550px)] md:before:w-[min(calc(100vw*0.47),500px)] md:before:h-[min(calc(100vw*0.47),500px)] lg:before:w-[min(calc(100vw*0.42),525px)] lg:before:h-[min(calc(100vw*0.42),525px)] *:-z-1 border-2 border-white`}
 		>
-			<img className="grayscale-100 w-6/7" src={img} />
+			<img className="grayscale-100 w-6/7 sm:w-[max(80vw,650px)] md:w-6/7" src={img} />
 		</div>
 	)
 }
