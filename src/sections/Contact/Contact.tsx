6b494d979f964b23b8746a0d2fe673d0e50388f4
PB <pbarna389@gmail.com>
2025-09-05 00:41:04 +0200

    08/footer - moved padding to sections

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -6,7 +6,7 @@ export const Contact = () => {
 	return (
 		<section
 			id="contact"
-			className="flex flex-col gap-8 justify-center items-center relative sm:justify-between"
+			className="flex flex-col gap-8 justify-center items-center relative sm:justify-between pt-6 pb-6 pr-4 pl-4 sm:pt-8 sm:pb-8 sm:pl-6 sm:pr-6 border-2 border-white"
 		>
 			<SectionHeader
 				title={'Contact'}
--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
 	return (
-		<footer className="flex flex-col gap-8 justify-center items-center relative sm:justify-between bg-[#FFF]/4">
+		<footer className="flex flex-col gap-8 justify-center items-center relative sm:justify-between bg-[#FFF]/4 w-full mt-6 pr-4 pl-4 sm:mt-8 sm:pl-6 sm:pr-6">
 			Footer
 		</footer>
 	)
