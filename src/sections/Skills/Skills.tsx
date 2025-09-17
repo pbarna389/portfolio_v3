2cb317f6586417436f5e508d009ac683e102f293
PB <pbarna389@gmail.com>
2025-09-17 14:07:42 +0200

    main - added background to skills

--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -1,5 +1,5 @@
 import { useTextContext } from '@context'
-import { IconCollection } from '@components'
+import { Background, IconCollection } from '@components'
 import { useIntersectionObserver } from '@hooks'
 
 import { Skill } from './Skill'
@@ -18,6 +18,7 @@ export const Skills = () => {
 			className={`flex flex-col gap-4 justify-center items-center relative sm:gap-10 pt-6 pb-6 pr-4 pl-4 sm:pt-8 sm:pb-8 sm:pl-6 sm:pr-6 transition duration-500 blur-2xl ${inView && 'blur-none'}`}
 		>
 			<h1 className="text-lg font-bold text-white lg:text-4xl">Skills</h1>
+			<Background />
 			<IconCollection
 				arraySource={skills}
 				size={50}
