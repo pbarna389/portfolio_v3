57c9af702c553eb7f9ba1750ab14f496966af050
PB <pbarna389@gmail.com>
2025-09-23 00:35:24 +0200

    20/wrapUp - fixed about section's picture

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -21,10 +21,10 @@ export const About = () => {
 			className={`flex flex-col gap-8 justify-center items-center relative sm:justify-between pt-6 pb-6 pr-4 pl-4 sm:pt-8 sm:pb-8 sm:pl-6 sm:pr-6  w-full transition duration-500 blur-2xl ${inView && 'blur-none'}`}
 		>
 			<SectionHeader title={'About Me'} subText={aboutMe.heading} />
-			<div className="sm:flex sm:flex-row sm:gap-10 w-full">
+			<div className="flex flex-col-reverse sm:flex-row sm:gap-10 w-full items-center">
 				<ProfilePic
 					src={img}
-					className='sm:flex sm:items-center sm:justify-end before:rounded-t-full before:rounded-b-none before:absolute before:bottom-0 before:w-[min(calc(100vw*0.85),600px)] before:h-[min(calc(100vw*0.925),800px)] before:content-[""] before:bg-white/4 before:-z-1 sm:before:w-[min(calc(100vw*0.42),550px)] sm:before:h-[min(calc(100vw*0.52),550px)] md:before:w-[min(calc(100vw*0.43),500px)] md:before:h-[min(calc(100vw*0.445),600px)] md:before:h-max-[700px] lg:before:w-[min(calc(100vw*0.42),525px)] lg:before:max-w-[500px] lg:before:max-h-[700px] lg:before:h-[max(calc(100vw*0.5),590px)] basis-1/2'
+					className='sm:flex sm:items-center sm:justify-end before:rounded-t-full before:rounded-b-none before:absolute before:bottom-0 before:w-[min(calc(100vw*0.85),600px)] before:h-[min(calc(100vw*0.925),800px)] before:content-[""] before:bg-white/4 before:-z-1 sm:before:w-[min(calc(100vw*0.42),550px)] sm:before:h-[min(calc(100vw*0.52),550px)] md:before:w-[min(calc(100vw*0.43),500px)] md:before:h-[min(calc(100vw*0.445),600px)] md:before:h-max-[700px] lg:before:w-[min(calc(100vw*0.42),5600px)] lg:before:max-w-[560px] lg:before:max-h-[700px] lg:before:h-[max(calc(100vw*0.5),590px)] basis-1/2'
 				/>
 				<div className="relative basis-1/2 flex flex-col justify-center items-center gap-4 sm:flex sm:items-start sm:gap-4  p-4">
 					<Background />
