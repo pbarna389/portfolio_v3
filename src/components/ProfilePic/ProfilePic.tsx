5a1340c5e1fa91e9a50ad0799ce8e49f3e64b286
PB <pbarna389@gmail.com>
2025-09-22 19:55:21 +0200

    18/submitBtnPopover - removed helper classes

--- src/components/ProfilePic/ProfilePic.tsx
+++ src/components/ProfilePic/ProfilePic.tsx
@@ -9,7 +9,7 @@ export const ProfilePic = ({ src, className = '' }: ProfilePicProps) => {
 			className={`flex flex-col relative basis-1/2 justify-end items-center overflow-hidden ${className}`}
 		>
 			<img
-				className="w-6/7 sm:relative sm:w-[max(80vw,650px)] md:w-6/7 xl:w-[550px] sm:grayscale-100 sm:hover:grayscale-0 transition ease-in-out duration-700"
+				className="w-6/7 sm:relative sm:w-[max(80vw,650px)] md:w-6/7 xl:w-[550px] sm:opacity-50 sm:hover:opacity-100 sm:grayscale-100 sm:hover:grayscale-0 transition ease-in-out duration-700"
 				src={src}
 				alt="picture about me"
 			/>
